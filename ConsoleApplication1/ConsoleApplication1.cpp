3c0a31e81510a035c0f9b375b58f4b8950fdd9c0
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-03 14:32:34 +0600

    Magnets

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -60,7 +60,7 @@ int main() {
 
 //presents
 
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main() {
 	int a, b, num[100];
@@ -72,4 +72,27 @@ int main() {
 	for (int i = 1; i <= a; i++) {
 		cout << num[i] << " ";
 	}
+}*/
+
+//Magnets
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+	int a, c=0;
+	cin >> a;
+	int arr[100000];
+	for (int i = 0; i < a; i++) 
+	{
+		cin >> arr[i];	
+	}
+	for (int i = 0;i < a;i++) {
+		if (arr[i] != arr[i + 1]) {
+			c++;
+		}
+	}
+	cout << c;
 }
+
+
