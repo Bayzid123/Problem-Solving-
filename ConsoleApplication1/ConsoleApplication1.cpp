e320b14dad2e56ea62de01108cec1d4a08b346fe
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-07 09:27:33 +0600

    Calculating Function

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -136,3 +136,21 @@ int main()
 	
 }*/
 
+// Calculating Function
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	long long a, b;
+	cin >> a;
+	if (a % 2 == 0) {
+		b = a / 2;
+	}
+	else {
+		b = ((a + 1) / 2) * (-1);
+	}
+	cout << b;
+}*/
+
+
