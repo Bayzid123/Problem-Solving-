b1871ef100db040954f1198960dae851ec84fe7f
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-07 18:08:58 +0600

    Is your horseshoe on the other hoof?

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -190,7 +190,7 @@ int main()
 
 // Hulk
 
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -210,4 +210,25 @@ int main()
 			cout << "it ";
 		}
 	}
+}*/
+
+// Is your horseshoe on the other hoof?
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int arr[4], count = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + 4);
+	for (int i = 0; i < 3; i++)
+	{
+		if (arr[i] == arr[i + 1])
+		{
+			count++;
+		}
+	}
+	cout << count;
 }
