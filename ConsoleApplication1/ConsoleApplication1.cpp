c2778c64b603bd8cbb2e52111175d76eac82cba1
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-15 18:00:16 +0600

    Kefa and First Steps

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -279,7 +279,7 @@ int main() {
 	}
 }*/
 // Taxi Right
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -305,4 +305,26 @@ int main()
 	}
 	cout << total;
 	return 0;
+}*/
+
+// Kefa and First Steps
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n, c=0, m=1;
+	cin >> n;
+	int a[100000];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (a[i] >= a[i - 1]) {
+			c++;
+			m = max(m, c);
+		}
+		else {
+			c = 1;
+		}		
+	}
+	cout << m;
 }
