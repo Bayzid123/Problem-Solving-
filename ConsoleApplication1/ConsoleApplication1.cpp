10ec97e66d3023d8a2026e6bd626fb05589c61dc
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-21 12:07:02 +0600

    Insomnia cure

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -392,7 +392,7 @@ int main() {
 
 // cAPS lOCK
 
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main() {
 	string s;
@@ -425,4 +425,20 @@ int main() {
 	else {
 		cout << s;
 	}
+}*/
+
+// Insomnia cure
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int k, l, m, n, d, c=0;
+	cin >> k >> l >> m >> n>>d;
+	for (int i = 1; i <= d; i++) {
+		if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0 ) {
+			c++;
+		}
+	}
+	cout << c;
 }
+
