7e00bba2f22b884070b1e9717fa8c97cd7b48af9
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-03-22 16:19:50 +0600

    Arrival of the General

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -444,7 +444,7 @@ int main() {
 
 // Dubstep
 
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main() {
 	string s;
@@ -458,4 +458,27 @@ int main() {
 			cout << s[i];
 		}
 	}
+}*/
+
+// Arrival of the General
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n, arr[100],ma=0,mi=101,max,min;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] > ma) {
+			ma = arr[i];
+			max = i;
+		}
+		if(arr[i] <= mi) {
+			mi = arr[i];
+			min = i;
+		}
+	}
+	if (max > min) {
+		min++;
+	}
+	cout << max + (n - 1) - min;
 }
